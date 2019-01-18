07fbf51a368f72df1de8f1bedf4e1d20bf3a9ea3
Vivek Rai <vivekraiiosdev@gmail.com>
2019-01-19 01:09:06 +0530

    UISplitViewController projecct

--- Splitter/DetailViewController.swift
+++ Splitter/DetailViewController.swift
@@ -9,31 +9,20 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
-    }
-
+    
+    @IBOutlet weak var imageItem: UIImageView!
+    
+    var image: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        configureView()
+        
+        imageItem.image = image
+        
     }
+    
 
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            configureView()
-        }
-    }
+   
 
 
 }
---
+++ Splitter/ImagesNamesColors.swift
@@ -0,0 +1,23 @@
+//
+//  ImagesNamesColors.swift
+//  Splitter
+//
+//  Created by Vivek Rai on 18/01/19.
+//  Copyright © 2019 Vivek Rai. All rights reserved.
+//
+
+import UIKit
+
+let camera = UIImage(named: "1")
+let city = UIImage(named: "2")
+let buffalo = UIImage(named: "3")
+let bouquet = UIImage(named: "4")
+let stand = UIImage(named: "5")
+let urban = UIImage(named: "6")
+
+let imagesArray = [camera, city, buffalo, bouquet, stand, urban]
+let namesArray = ["CAMERA", "CITY", "ANIMALS", "FLOWERS", "STAND", "URBAN"]
+let colorsArray = [#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1), #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)]
+
+
+
